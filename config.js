360eb21095c2ce5a8a7a7587162439bca1c0d9ce
olustrrax <olutrrax@gmail.com>
2018-01-03 17:41:15 +0700

    first push

---
+++ config.js
@@ -0,0 +1,14 @@
+
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'EsamCBkKq1mmWh6ixeQgntX9X',  
+  consumer_secret: 'jQzMCplmeuaeRUK9matFg2A7JyQQFFFcTEd7vcgyAcfPqEFVir',
+  access_token: '948480623223566337-pp0BTvLOaVKreNbysnAYq3c4N5RE2m9',  
+  access_token_secret: 'OREtSuNjHIW8vcLDMZejKENlwHFEd1EvRrBAUsE8lKc8m'
+}
