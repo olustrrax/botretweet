c5ea7c67cf3f6c2cdd5ad02ddde084f641f974b8
dan's cat <olutrrax@gmail.com>
2018-04-22 02:40:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
 
-//config.js
-/** TWITTER APP CONFIGURATION
- * consumer_key
- * consumer_secret
- * access_token
- * access_token_secret
- */
-module.exports = {
-  consumer_key: 'cfrdWtEosASFbgWVgzhLmHODG',  
-  consumer_secret: 'MRHqOCxbQxJZguNWysWZWfzkSNZvoEPAxsnP48hpRKwCuJrJ4Z',
-  access_token: '948480623223566337-d0w3GqKy4sIfW1yw5ybsZasEFc8TCSQ',  
-  access_token_secret: 'NLgsYkuTi524f26sr4l0t6GNexK77JMUmI6lp4blHM2p8'
-}
-=======
 
 //config.js
 /** TWITTER APP CONFIGURATION
@@ -28,4 +13,4 @@ module.exports = {
   access_token: '',  
   access_token_secret: ''
 }
->>>>>>> 9eda5aefdc175e92de616a5df933ab6a0f80b8a4
+
